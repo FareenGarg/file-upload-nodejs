a72a9691130f5da0ca4b3c11d599bcce13fdc99a
Fareen Garg <fareengarg13092005@gmail.com>
2026-02-21 16:43:32 +0530

    Updated File model

---
+++ models/File.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const fileSchema = new mongoose.Schema(
+    {
+        name : 
+        {
+            type : String,
+            required : true,
+        },
+        imageUrl : 
+        {
+            type : String,
+        },
+        tags : 
+        {
+            type : String,
+        },
+        imageUrl : 
+        {
+            type : String,
+        },
+        email : 
+        {
+            type : String,
+        },
+    }
+)
